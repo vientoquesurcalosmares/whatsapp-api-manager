d06062915ce0f1a29c50d4e8c2dd738ca1205acf
Wilfredo\Wilfredo Perilla <wilfredoperilla@gmail.com>
2025-04-28 15:05:06 -0500

    Fix bugs

--- src/Database/Migrations/2024_12_17_195801_create_messages_table.php
+++ src/Database/Migrations/2024_12_17_195801_create_messages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('whatsapp_messages', function (Blueprint $table) {
             $table->ulid('message_id')->primary();
             $table->ulid('whatsapp_phone_id');
             $table->ulid('contact_id');
@@ -66,6 +66,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('whatsapp_messages');
     }
 };
