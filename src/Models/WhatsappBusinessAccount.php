b159dc6d42061243c23121a0febec079ef42e772
Wilfredo\Wilfredo Perilla <wilfredoperilla@gmail.com>
2025-04-28 11:32:25 -0500

    Fix bugs

--- src/Models/WhatsappBusinessAccount.php
+++ src/Models/WhatsappBusinessAccount.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use ScriptDevelop\WhatsappManager\Traits\GeneratesUlid;
+
 class WhatsappBusinessAccount extends Model
 {
     use HasFactory, SoftDeletes;
+    use GeneratesUlid;
 
     protected $primaryKey = 'whatsapp_business_id';
     public $incrementing = false;
