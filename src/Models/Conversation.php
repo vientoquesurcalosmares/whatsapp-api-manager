f4f3f51675b767a188ee8814e4622bf34d30b007
vientoquesurcalosmares <41802038+vientoquesurcalosmares@users.noreply.github.com>
2025-05-29 11:41:20 -0600

    Update Conversation.php attribute $table
    Add attribute $table

--- src/Models/Conversation.php
+++ src/Models/Conversation.php
@@ -13,6 +13,13 @@ class Conversation extends Model
     use HasFactory, SoftDeletes;
     use GeneratesUlid;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'conversations';
+
     protected $primaryKey = 'conversation_id';
     public $incrementing = false;
     protected $keyType = 'string';
