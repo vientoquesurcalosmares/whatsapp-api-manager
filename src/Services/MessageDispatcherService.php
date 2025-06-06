145eb94c09caa35b11d97d4e79efaa708755f586
vientoquesurcalosmares <41802038+vientoquesurcalosmares@users.noreply.github.com>
2025-06-06 16:35:01 -0600

    Update MessageDispatcherService.php resolveContact
    Se agrega código especial para el caso de México, es el único país del mundo según ChatGPT que después del código de area 52 agrega 1 y luego los 10 dígitos del celular, ese 1 se usa cuando es llamada internacional y significa que es un celular, desde el interior de la República Mexicana es 1 se omite, pero para estandarizarlo se pondrá siempre ese 1

--- src/Services/MessageDispatcherService.php
+++ src/Services/MessageDispatcherService.php
@@ -3104,6 +3104,13 @@ class MessageDispatcherService
      */
     private function resolveContact(string $countryCode, string $phoneNumber): Contact
     {
+        //Si el país es México, según ChatGPT este es el único caso en el mundo que tiene un 1 después del código de area y luego vienen 10 dígitos del celular así 521 1234567890
+        //Por lo tanto comprobar si es número de méxico y el $phoneNumber son exactamente 10 números, entonces agregar el 1 inicial
+        if( $countryCode==52 && Str::length($phoneNumber)==10 )
+        {
+            $phoneNumber = '1'.$phoneNumber;
+        }
+
         $fullPhoneNumber = $countryCode . $phoneNumber;
 
         Log::channel('whatsapp')->info('Resolviendo contacto.', ['full_phone_number' => $fullPhoneNumber]);
