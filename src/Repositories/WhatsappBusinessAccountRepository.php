7a539450f67d5b4c2b654148402d9cd540ea15e1
c_torres <vientoquesurcalosmares@gmail.com>
2025-06-17 16:07:31 -0600

    Adaptando clase WhatsappModelResolver

--- src/Repositories/WhatsappBusinessAccountRepository.php
+++ src/Repositories/WhatsappBusinessAccountRepository.php
@@ -2,17 +2,20 @@
 
 namespace ScriptDevelop\WhatsappManager\Repositories;
 
-use ScriptDevelop\WhatsappManager\Models\WhatsappBusinessAccount;
+//use ScriptDevelop\WhatsappManager\Models\WhatsappBusinessAccount;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+use Illuminate\Database\Eloquent\Model;
+use ScriptDevelop\WhatsappManager\Support\WhatsappModelResolver;
 
 class WhatsappBusinessAccountRepository
 {
     /**
      * Encuentra una cuenta empresarial por ID.
      */
-    public function find(string $id): WhatsappBusinessAccount
+    public function find(string $id): Model
     {
-        $account = WhatsappBusinessAccount::find($id);
+        $account = WhatsappModelResolver::business_account()->find($id);
 
         if (!$account) {
             throw new ModelNotFoundException("Whatsapp Business Account $id no encontrada");
@@ -24,8 +27,8 @@ class WhatsappBusinessAccountRepository
     /**
      * Encuentra una cuenta por número de teléfono.
      */
-    public function findByPhoneNumberId(string $phoneNumberId): WhatsappBusinessAccount
+    public function findByPhoneNumberId(string $phoneNumberId): Model
     {
-        return WhatsappBusinessAccount::where('phone_number_id', $phoneNumberId)->firstOrFail();
+        return WhatsappModelResolver::business_account()->where('phone_number_id', $phoneNumberId)->firstOrFail();
     }
 }
